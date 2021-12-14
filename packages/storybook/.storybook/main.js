5de47620c1d9032c3e5d921c3a0a66c814c61131
Luke Watts <luke@thisis.la>
2021-12-14 20:12:26 +0100

    🤷🏻‍♂️

--- packages/storybook/.storybook/main.js
+++ packages/storybook/.storybook/main.js
@@ -7,16 +7,24 @@ module.exports = {
   stories: ['../../../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   // https://github.com/storybookjs/presets/tree/master/packages/preset-create-react-app
   addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-knobs',
-    'storybook/preset-create-react-app',
+    {
+      name: '@storybook/preset-create-react-app',
+      options: {
+        tsDocgenLoaderOptions: {
+          tsconfigPath: path.resolve(__dirname, '../tsconfig.json'),
+        },
+      },
+    },
     {
       name: '@storybook/addon-docs',
       options: {
         configureJSX: true,
       },
     },
+    '@storybook/addon-actions',
+    '@storybook/addon-essentials',
+    '@storybook/addon-links',
+    '@storybook/addon-knobs',
   ],
   // https://storybook.js.org/docs/react/configure/typescript
   typescript: {},
@@ -24,7 +32,10 @@ module.exports = {
   // https://webpack.js.org/configuration/resolve/#resolve-modules
   // https://storybook.js.org/docs/react/configure/webpack
   webpackFinal: async config => {
-    config.resolve.modules = [paths.appSrcDir, ...(config.resolve.modules || [])]
+    config.resolve.modules = [
+      paths.appSrcDir,
+      ...(config.resolve.modules || []),
+    ]
     return config
   },
 }
