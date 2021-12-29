3a71f5164bc7533f218d15a850067dbd3be61bb8
Luke Watts <luke@thisis.la>
2021-12-29 07:47:40 +0100

    wip

--- src/components/Form/FlagSelect.tsx
+++ src/components/Form/FlagSelect.tsx
@@ -1,14 +1,14 @@
+import type { IFieldProps } from './Fields'
 import { FieldContainer, ErrorMessage } from './elements'
 import ReactFlagsSelect from 'react-flags-select'
 import { Flex } from 'rebass/styled-components'
-import { IFieldProps } from './Fields'
 
 export const FlagSelectField = ({ input, meta }: IFieldProps) => (
   <Flex p={0} flexWrap="wrap">
     <FieldContainer invalid={meta.error && meta.touched}>
       <ReactFlagsSelect
         searchable={true}
-        defaultCountry={'input.value'}
+        selected={'input.value'}
         onSelect={v => {
           input.onChange(v)
           input.onBlur()
