5aa9112d810ab098cd87c0f232d777f53820285a
Luke Watts <luke@thisis.la>
2022-01-21 13:06:35 +0100

    test: update assertations to support filename with timestamp

--- packages/cypress/src/integration/howto/write.spec.ts
+++ packages/cypress/src/integration/howto/write.spec.ts
@@ -1,9 +1,6 @@
-const testTime = Date.now()
-
 describe('[How To]', () => {
   beforeEach(() => {
-    cy.clock(testTime)
-      .visit('/how-to')
+    cy.visit('/how-to')
   })
   type Duration = '<1 week' | '1-2 weeks' | '3-4 weeks'
   type Difficulty = 'Easy' | 'Medium' | 'Hard' | 'Very Hard'
